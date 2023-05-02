b3274fe5df9d85af5b6f783c1676ee20a57a7e2b
김윤우 <ynwookim98@gmail.com>
2023-05-02 15:02:49 +0900

    controller 오류 수정!!!

--- src/main/java/com/ssafy/enjoytrip/user/controller/UserController.java
+++ src/main/java/com/ssafy/enjoytrip/user/controller/UserController.java
@@ -3,19 +3,16 @@ package com.ssafy.enjoytrip.user.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.enjoytrip.user.model.UserDto;
 import com.ssafy.enjoytrip.user.model.service.UserService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/user")
@@ -31,16 +28,17 @@ public class UserController {
 	}
 //	@RequestBody UserDto userDto
 	@PostMapping("/join")
-	public ResponseEntity<?> userJoin(@RequestBody UserDto userDto) {
+	public void userJoin(@RequestBody UserDto userDto) {
 		System.out.println(userDto);
-		logger.debug("유저 등록 UserDto : {}", userDto);
+//		System.out.println(userDto);
+//		logger.debug("유저 등록 UserDto : {}", userDto);
 		
-		try {
-			userService.joinUser(userDto);
-			return new ResponseEntity<String>("회원가입 완료!!!", HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<String>("회원가입 실패!!!", HttpStatus.NOT_ACCEPTABLE);
-		}
+//		try {
+//			userService.joinUser(userDto);
+//			return new ResponseEntity<String>("회원가입 완료!!!", HttpStatus.OK);
+//		} catch (Exception e) {
+//			return new ResponseEntity<String>("회원가입 실패!!!", HttpStatus.NOT_ACCEPTABLE);
+//		}
 	}
 	
 	@GetMapping("/login/{id}")
--- src/main/java/com/ssafy/enjoytrip/user/model/UserDto.java
+++ src/main/java/com/ssafy/enjoytrip/user/model/UserDto.java
@@ -1,59 +1,70 @@
-	package com.ssafy.enjoytrip.user.model;
-	
-	public class UserDto {
-		private String id;
-		private String password;
-		private String email;
-		private String name;
-		private int age;
-	
-	
-		public String getId() {
-			return id;
-		}
-	
-		public void setId(String id) {
-			this.id = id;
-		}
-	
-		public String getPassword() {
-			return password;
-		}
-	
-		public void setPassword(String password) {
-			this.password = password;
-		}
-	
-		public String getEmail() {
-			return email;
-		}
-	
-		public void setEmail(String email) {
-			this.email = email;
-		}
-	
-		public String getName() {
-			return name;
-		}
-	
-		public void setName(String name) {
-			this.name = name;
-		}
-	
-		public int getAge() {
-			return age;
-		}
-	
-		public void setAge(int age) {
-			this.age = age;
-		}
-	
-		@Override
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			builder.append("UserDto [id=").append(id).append(", password=").append(password).append(", email=")
-					.append(email).append(", name=").append(name).append(", age=").append(age).append("]");
-			return builder.toString();
-		}
-	
+package com.ssafy.enjoytrip.user.model;
+
+public class UserDto {
+	private String id;
+	private String password;
+	private String email;
+	private String name;
+	private int age;
+
+	public UserDto() {
+
 	}
+
+	public UserDto(String id, String password, String email, String name, int age) {
+		this.id = id;
+		this.password = password;
+		this.email = email;
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("UserDto [id=").append(id).append(", password=").append(password).append(", email=")
+				.append(email).append(", name=").append(name).append(", age=").append(age).append("]");
+		return builder.toString();
+	}
+
+}
